da29ef32adb7d85b8a35c20d901c59055dc59d43
Lalit <lalit.patil5694@gmail.com>
2021-07-16 11:28:11 +0530

    Add Create Room Page and Material UI

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,12 +1,42 @@
 from django.http.request import HttpHeaders
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from rest_framework.serializers import Serializer
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            vote_to_skip = serializer.data.get('vote_to_skip')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            print("queryset: ", queryset)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.vote_to_skip = vote_to_skip
+                room.save(update_fields=['guest_can_pause', 'vote_to_skip'])
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            vote_to_skip=vote_to_skip)
+                room.save()
+
+            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
