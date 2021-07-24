aac081c183cc41df95d0dbffc7e92cb099ac3476
Lalit <lalit.patil5694@gmail.com>
2021-07-24 13:35:33 +0530

    Add Room and Leave Room Function

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView
+from .views import GetRoom, JoinRoom, LeaveRoom, RoomView, CreateRoomView, UserInRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view())
 ]
